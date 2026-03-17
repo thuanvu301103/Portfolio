38d175b47e720c0ebdd3ef148f8b8de859f4610b
thuanvu301103 <96073973+thuanvu301103@users.noreply.github.com>
2026-03-17 17:23:27 +0700

    Feat/hero section: Implement Hero Section (#2)
    * feat/hero-section: Implement SystemCore Cat interact
    
    * feat/hero-section: Complete Hero Section
    
    * feat/hero-section: Update link information
    
    ---------
    
    Co-authored-by: ThuanVu <thuan.vu.e@ctmcorp.com.vn>

--- my-portfolio/app/page.tsx
+++ my-portfolio/app/page.tsx
@@ -1,19 +1,14 @@
-import { ThemeToggle } from "@/components/theme-toggle"
+import { Navbar } from "@/components/layout/navbar";
+import { HeroSection } from "@/components/section/hero-section";
 
 export default function Home() {
   return (
-    <main className="min-h-screen transition-colors duration-300 bg-white dark:bg-zinc-950 text-zinc-900 dark:text-zinc-50">
-      <nav className="p-6 flex justify-between items-center border-b dark:border-zinc-800">
-        <span className="font-bold text-xl">Portfolio.</span>
-        <ThemeToggle />
-      </nav>
-      
-      <section className="flex flex-col items-center justify-center pt-20">
-        <h1 className="text-5xl font-extrabold mb-4">Hello, I'm a Developer</h1>
-        <p className="text-zinc-500 dark:text-zinc-400">
-          This content changes color based on the theme!
-        </p>
-      </section>
-    </main>
-  )
+    <div className="min-h-screen bg-white transition-colors duration-300 dark:bg-zinc-950">
+      <Navbar />
+
+      <main className="mx-auto max-w-7xl px-6 pt-12 md:pt-12">
+        <HeroSection />
+      </main>
+    </div>
+  );
 }
