08a7c849f58f62f8bb001dfb436a24824e7d1cd7
thuanvu301103 <96073973+thuanvu301103@users.noreply.github.com>
2026-03-19 00:42:27 +0700

    feat/experience: Implement Experience Section (#8)

--- my-portfolio/app/page.tsx
+++ my-portfolio/app/page.tsx
@@ -1,5 +1,6 @@
 import { Navbar } from "@/components/layout/navbar";
 import { AboutSection } from "@/components/section/about-section";
+import { WorkExperience } from "@/components/section/experience-section";
 import { HeroSection } from "@/components/section/hero-section";
 import { ProjectsSection } from "@/components/section/project-section";
 import { TechStack } from "@/components/section/tech-stack-section";
@@ -24,6 +25,10 @@ export default function Home() {
 
         <section id="projects" className="snap-start pt-12">
           <ProjectsSection />
+        </section>
+
+        <section id="experience" className="snap-start pt-12">
+          <WorkExperience />
         </section>
       </main>
     </div>
