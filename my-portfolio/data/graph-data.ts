165b7f755aaf40e632d025f420ea0f9d5339d3f0
thuanvu301103 <96073973+thuanvu301103@users.noreply.github.com>
2026-04-08 08:47:24 +0700

    data/graph: Add Graph Node Programming Language (#26)
    * feat/certification: Implement Cert Section (#14)
    
    * Improve UI (#16)
    
    Co-authored-by: ThuanVu <thuan.vu.e@ctmcorp.com.vn>
    
    * data/graph: Add Golang TypeSystem node
    
    * fix/navbar: Add Mobile behaviour for mobile Navbar (#19)
    
    Co-authored-by: ThuanVu <thuan.vu.e@ctmcorp.com.vn>
    
    * data/graph: Add new node for Genkit
    
    * feat/graph: Implement Expanable Nodes (#21)
    
    Co-authored-by: ThuanVu <thuan.vu.e@ctmcorp.com.vn>
    
    * fix: Remove duplicate import and data
    
    * data/graph: Add GCP Node
    
    * feat/graph: Add graph node for Programming Language
    
    ---------
    
    Co-authored-by: ThuanVu <thuan.vu.e@ctmcorp.com.vn>

--- my-portfolio/data/graph-data.ts
+++ my-portfolio/data/graph-data.ts
@@ -2,6 +2,14 @@ export const graphData = {
   nodes: [
     {
       id: "1",
+      title: "Programming Language",
+      description: ``,
+      size: 8,
+      url: "",
+      group: 1,
+    },
+    {
+      id: "1.1",
       title: "Golang",
       description: `Golang (Go) is designed for simplicity, high performance, and efficient concurrency. 
               It features fast compilation, a powerful standard library, and lightweight goroutines`,
@@ -10,7 +18,7 @@ export const graphData = {
       group: 1,
     },
     {
-      id: "1.1",
+      id: "1.1.1",
       title: "Golang Runtime",
       description: `A core engine responsible for managing program execution, 
         handling Goroutine scheduling (concurrency), automatic memory management (Garbage Collection), 
@@ -20,13 +28,81 @@ export const graphData = {
       group: 1,
     },
     {
-      id: "1.2",
+      id: "1.1.2",
       title: "Golang TypeSystem",
       description: `An in-depth exploration of Go's type system, 
         focusing on how Structs and Interfaces implement OOP principles like Encapsulation and Polymorphism 
         through composition rather than inheritance.`,
       size: 2,
       url: "https://medium.com/@vungocthuan1234/go-type-system-struct-and-interface-an-oop-approach-11c403fd32f8",
+      group: 1,
+    },
+    {
+      id: "1.2",
+      title: "JavaScript",
+      description: `JavaScript is a high-level programming language mainly used to create interactive 
+        and dynamic content on websites, such as animations, forms, and real-time updates.`,
+      size: 6,
+      url: "",
+      group: 1,
+    },
+    {
+      id: "1.2.1",
+      title: "Asynchronous Syntax",
+      description: `Asynchronous syntax in JavaScript allows code to handle long - running tasks 
+        without blocking the main execution thread. It enables programs to stay responsive 
+        by executing tasks in the background and handling results later, 
+        commonly using callbacks, Promises, and async/await for cleaner and more manageable code.`,
+      size: 2,
+      url: "https://drive.google.com/file/d/1cHV-HwDhUeBQkXyKWQSaD3UFNvheBLP_/view?usp=sharing",
+      group: 1,
+    },
+    {
+      id: "1.2.2",
+      title: "NodeJS Runtime",
+      description: `Node.js Runtime is a server-side JavaScript runtime built on Google's V8 engine 
+        that allows JavaScript to run outside the browser. It is designed for building fast, scalable, 
+        and efficient network applications using a non-blocking, event-driven architecture.`,
+      size: 2,
+      url: "https://drive.google.com/file/d/1XyeaZ7u154YW41N763FWdUwqfJNO2mvh/view?usp=drive_link",
+      group: 1,
+    },
+    {
+      id: "1.2.3",
+      title: "ReactJS",
+      description: `ReactJS is a JavaScript library used to build fast and interactive user interfaces by creating reusable, 
+        component-based UI elements.`,
+      size: 2,
+      url: "https://drive.google.com/drive/folders/1gz27fEg0kxSrzyRllifJV_bzUWAvbG1R?usp=sharing",
+      group: 1,
+    },
+    {
+      id: "1.3",
+      title: "TypeScript",
+      description: `TypeScript is a superset of JavaScript that adds static typing, helping developers catch errors early 
+        and build more reliable, maintainable applications—especially for large-scale projects.`,
+      size: 6,
+      url: "",
+      group: 1,
+    },
+    {
+      id: "1.3.1",
+      title: "Type Programming",
+      description: `Using TypeScript's advanced type system (such as generics, conditional types, and type inference) 
+        to model logic at the type level, improving code safety, expressiveness, and maintainability 
+        without affecting runtime behavior.`,
+      size: 2,
+      url: "https://drive.google.com/file/d/1REPBvcWH6Rz2-dV078QiZf9An8Xdqtzl/view?usp=sharing",
+      group: 1,
+    },
+    {
+      id: "1.3.2",
+      title: "Transpilation",
+      description: `Process of converting TypeScript code into plain JavaScript, 
+        allowing it to run in browsers or JavaScript runtimes while preserving compatibility and 
+        adding type safety during development`,
+      size: 2,
+      url: "https://drive.google.com/file/d/1QsXmS8_vYOolJZoh_de2Qz4oLyKqMxzQ/view?usp=sharing",
       group: 1,
     },
     {
@@ -159,11 +235,37 @@ export const graphData = {
       url: "https://docs.cloud.google.com/compute/docs/overview",
       group: 3,
     },
+    {
+      id: "5.1.3",
+      title: "Cloud Run",
+      description: `Cloud Run is a fully managed application platform for running your code, 
+      function, or container on top of Google's highly scalable infrastructure.`,
+      size: 2,
+      url: "https://docs.cloud.google.com/run/docs/overview/what-is-cloud-run",
+      group: 3,
+    },
+    {
+      id: "5.1.4",
+      title: "Compute Engine",
+      description: `Compute Engine is an infrastructure as a service (IaaS) 
+      product that offers self-managed virtual machine (VM) instances and bare metal instances`,
+      size: 2,
+      url: "https://docs.cloud.google.com/compute/docs/overview",
+      group: 3,
+    },
   ],
   links: [
     { source: "1", target: "1.1" },
     { source: "1", target: "1.2" },
-    { source: "1", target: "1.2" },
+    { source: "1", target: "1.3" },
+    { source: "1.1", target: "1.1.1" },
+    { source: "1.1", target: "1.1.2" },
+    { source: "1.2", target: "1.2.1" },
+    { source: "1.2", target: "1.2.2" },
+    { source: "1.2", target: "1.2.3" },
+    { source: "1.2", target: "1.3" },
+    { source: "1.3", target: "1.3.1" },
+    { source: "1.3", target: "1.3.2" },
     { source: "2", target: "2.1" },
     { source: "4", target: "4.1" },
     { source: "4", target: "4.2" },
